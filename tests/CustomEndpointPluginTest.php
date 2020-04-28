77b7c519ec9d2716cee8ac7cad7d19f7640275a8
Mario R <mariort@outlook.com>
2020-04-28 15:43:13 +0200

    update tests

--- tests/CustomEndpointPluginTest.php
+++ tests/CustomEndpointPluginTest.php
@@ -8,6 +8,7 @@ use InpsydeTest\CustomEndpointPlugin;
 use InpsydeTest\Util\RouteExtractor;
 use InpsydeTest\EndpointHandler\TestEndpointHandler;
 use Psr\Http\Message\ResponseInterface;
+use InpsydeTest\ErrorHandler\GenericErrorHandler;
 
 class CustomEndpointPluginTest extends TestCase
 {
@@ -56,12 +57,50 @@ class CustomEndpointPluginTest extends TestCase
 
         $this->containerMock->expects($this->once())
         ->method('get')
-        ->with(InpsydeTest\EndpointHandler\TestEndpointHandler::class)
+        ->with(TestEndpointHandler::class)
         ->willReturn($handlerMock);
 
         $this->emitterMock->expects($this->once())
         ->method('emit')
         ->with($responsemock);
+
+        $this->testInstance->onRequest($wpMock);
+    }
+
+    public function testOnRequestExceptionHandler()
+    {
+        $wpMock = $this->createMock(WP::class);
+        $handlerMock = $this->createMock(TestEndpointHandler::class);
+        $errorHandlerMock = $this->createMock(GenericErrorHandler::class);
+
+        $this->routeExtractorMock->expects($this->once())
+        ->method('getRoute')
+        ->with($wpMock)
+        ->willReturn('inpsyde-test');
+
+
+        $handlerMock->expects($this->once())
+        ->method('handle')
+        ->will($this->throwException(new Exception()));
+
+        $errorHandlerMock->expects($this->once())
+        ->method('handleError')
+        ->with(
+            $this->isInstanceOf(Throwable::class)
+        );
+
+        $this->containerMock->expects($this->exactly(2))
+        ->method('get')
+        ->withConsecutive(
+            [TestEndpointHandler::class],
+            [GenericErrorHandler::class]
+        )
+        ->will(
+            $this->onConsecutiveCalls(
+                $handlerMock,
+                $errorHandlerMock
+            )
+        );
 
         $this->testInstance->onRequest($wpMock);
     }
